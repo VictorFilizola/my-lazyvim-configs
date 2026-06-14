e9272f4df51f95047b3c00312ebdcf09406951d8
Victor Filizola <victorblizzard@hotmail.com>
2026-06-14 18:08:49 -0300

    update: added functionality to remove highlighting

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+--
+-- this disables how text keeps being highlighted after /
+-- vim.opt.hlsearch = false
