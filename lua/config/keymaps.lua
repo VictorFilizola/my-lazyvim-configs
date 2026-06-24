91b25728b2b89199ceca3e4a50efb0b1b0b948e5
Victor Filizola <victorblizzard@hotmail.com>
2026-06-24 11:31:29 -0300

    feat: added bx command to delete all buffers except current one

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,4 +1,4 @@
--- Keymaps are automatically loaded on the VeryLazy event
+--  are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
@@ -17,3 +17,29 @@ vim.keymap.set("t", "<C-Down>", [[<C-\><C-n><C-w>j]], { desc = "Jump into Termin
 
 vim.keymap.set("n", "<S-Left>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
 vim.keymap.set("n", "<S-Right>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+
+-- Command for custom command 'bx' - it deletes all buffers except current buffer, terminals and file tree
+local delete_other_buffers = function()
+  local cur = vim.api.nvim_get_current_buf()
+  for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+    if vim.api.nvim_buf_is_valid(buf) and buf ~= cur then
+      local bt = vim.bo[buf].buftype
+      -- skip terminal, file explorers (nofile), quickfix, help
+      if bt == "" then
+        vim.api.nvim_buf_delete(buf, { force = false })
+      end
+    end
+  end
+end
+
+vim.keymap.set(
+  "n",
+  "<leader>bx",
+  delete_other_buffers,
+  { desc = "Delete all buffers except current, terminal, and filetree" }
+)
+vim.api.nvim_create_user_command(
+  "Bcloseothers",
+  delete_other_buffers,
+  { desc = "Close all normal buffers except current" }
+)
